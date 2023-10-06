12229e516b6ad05d19ef72576e557870e5f01ef6
AlbertLeipzig <albertclemvill@gmail.com>
2023-10-06 09:14:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+console.clear()
+
+const composers = [
+    {
+      firstName: 'Ludwig Van',
+      lastName: 'Beethoven',
+      birthYear: 1770,
+      deathYear: 1827,
+      worksComposed: 722,
+    },
+    {
+      firstName: 'Johann Sebastian',
+      lastName: 'Bach',
+      birthYear: 1685,
+      deathYear: 1750,
+      worksComposed: 1120,
+    },
+    {
+      firstName: 'Carl Phillip Emmanuel',
+      lastName: 'Bach',
+      birthYear: 1714,
+      deathYear: 1788,
+      worksComposed: 996,
+    },
+    {
+      firstName: 'Wolfgang Amadeus',
+      lastName: 'Mozart',
+      birthYear: 1756,
+      deathYear: 1791,
+      worksComposed: 600,
+    },
+    {
+      firstName: 'Johannes',
+      lastName: 'Brahms',
+      birthYear: 1833,
+      deathYear: 1897,
+      worksComposed: 198,
+    },
+    {
+      firstName: 'Paul',
+      lastName: 'Hindemith',
+      birthYear: 1895,
+      deathYear: 1963,
+      worksComposed: 884,
+    },
+  ];
---
+++ tasks.md
@@ -0,0 +1,12 @@
+This Repository is to review concepts from js:
+
+- [] Cloning
+- [] Destructuring
+- [] Higher order functions
+- [] for...in
+- [] Bracket notation
+- [] Object methods
+- [] JavaScript Class
+- [] The this keyword
+- [] Function as value
+- [] Callbacks // Function as value
